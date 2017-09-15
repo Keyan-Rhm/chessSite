3967a786dde2f7b6f99a458cb935ccfe5ea7d7ea
Keyan <keyan.raahemifar@gmail.com>
2017-09-14 23:29:04 -0400

    Starting 2 player gameplay implementations

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+var shortid = require('shortid');
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/static/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+var http = require('http').Server(app);
+var io = require('/usr/local/lib/node_modules/socket.io')(http);
+
+var players = 2;
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render('home');
 });
 
-app.get('/Multiplayer', function(req, res) {
-  res.sendFile(__dirname + '/index_multi.html');
+app.get('/playAI', function(req, res) {
+  res.render('index', {myTinyURL: shortid.generate(), player: req.query.player});
+});
+
+app.get('/:id', function(req, res) {
+  console.log("Memes" + req.params.id);
+  res.render('multi', {numPlayers: req.params.id, player: req.query.player});
+  console.log(players);
+  players -= 1;
 });
 
 io.on('connection', function(socket) {
